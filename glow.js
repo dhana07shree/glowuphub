7b5188ed394d36590f52417539c2b9bf31a07db7
Sharanya Gangala <you@example.com>
2025-09-06 14:50:06 +0530

    unnecessary

--- glow.js
+++ glow.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // hello kkgfg git config user.name"Tanishka"
 
 // Change
@@ -10,3 +9,5 @@ helo how are you hkjhjd HTMLObjectElements
 
 shradha is working
 >>>>>>> 77400e144338267c782084461ec6f4a91cdbc3f4
+ 
+new changes
